6afbfea9d7ca71f28a7d9f101621b3e861a4b6b1
Phillip Henry <PhillHenry@gmail.com>
2020-03-13 13:27:41 +0000

    Refactoring.

--- src/test/scala/uk/co/odinconsultants/sssplayground/DeltaPersistSpec.scala
+++ src/test/scala/uk/co/odinconsultants/sssplayground/DeltaPersistSpec.scala
@@ -11,31 +11,26 @@ class DeltaPersistSpec extends WordSpec with Matchers {
   import DeltaPersistSpec._
 
   "A typical dataframe" should {
+    val filename  = hdfsUri + this.getClass.getSimpleName
+    val words:    Seq[String]   = "the quick brown fox jumped over the lazy dog".split(" ")
     "be persisted as a delta frame" in {
-      val s               = SparkForTesting.session
-      import s.implicits._
-      val filename  = hdfsUri + this.getClass.getSimpleName
+      val write1:   Seq[MyRow]    = words.map(x => MyRow(x, 1))
+      val written1: Array[MyRow]  = writeBatch(filename, write1)
+      written1.toSet shouldBe write1.toSet
 
-      val words: Seq[String]        = "the quick brown fox jumped over the lazy dog".split(" ")
-      writeFirstBatch(s, filename, words)
-
-      info(s"Files after first bacth in $filename:\n${list(filename).mkString("\n")}")
-
-      val secondRows: Seq[MyRow] = words.map(x => MyRow(x, 2))
-      deltaWrite(s, secondRows, filename)
-      val fromDisk = s.read.parquet(filename).as[MyRow]
-
-      info(s"Files after second batch in $filename:\n${list(filename).mkString("\n")}")
-      fromDisk.collect should have size (words.size * 2)
+      val write2:   Seq[MyRow]    = words.map(x => MyRow(x, 2))
+      val written2: Array[MyRow]  = writeBatch(filename, write1)
+      written2 should have size (words.size * 2)
     }
   }
 
-  private def writeFirstBatch(s: SparkSession, filename: String, words: Seq[String]): Unit = {
+  private def writeBatch(filename: String, rows: Seq[MyRow]): Array[MyRow] = {
+    val s = SparkForTesting.session
     import s.implicits._
-    val firstRows: Seq[MyRow] = words.map(x => MyRow(x, 1))
-    deltaWrite(s, firstRows, filename)
+
+    deltaWrite(s, rows, filename)
     val fromDisk = s.read.parquet(filename).as[MyRow]
-    fromDisk.collect().toSet shouldBe firstRows.toSet
+    fromDisk.collect()
   }
 
   private def deltaWrite(s: SparkSession, xs: Seq[MyRow], filename: String): Unit = {
