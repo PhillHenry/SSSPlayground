0702209d261e147efa6ca3ca01c94883f7fb0f78
Phillip Henry <PhillHenry@gmail.com>
2020-03-13 05:20:15 +0000

    Two columns.

--- src/test/scala/uk/co/odinconsultants/sssplayground/DeltaPersistSpec.scala
+++ src/test/scala/uk/co/odinconsultants/sssplayground/DeltaPersistSpec.scala
@@ -7,26 +7,35 @@ import uk.co.odinconsultants.htesting.spark.SparkForTesting
 
 class DeltaPersistSpec extends WordSpec with Matchers {
 
+  import DeltaPersistSpec._
+
   "A typical dataframe" should {
     "be persisted as a delta frame" in {
       val s               = SparkForTesting.session
-
+      import s.implicits._
       val filename  = hdfsUri + this.getClass.getSimpleName
 
-      val first: Seq[String]        = "the quick brown fox jumped over the lazy dog".split(" ")
-      deltaWrite(s, first, filename)
+      val words: Seq[String]        = "the quick brown fox jumped over the lazy dog".split(" ")
+      val firstRows: Seq[MyRow] = words.map(x => MyRow(x, 1))
+      deltaWrite(s, firstRows, filename)
 
-      val fromDisk = s.read.parquet(filename)
-      fromDisk.collect().map(_.mkString("")).toSet shouldBe first.toSet
+      val fromDisk = s.read.parquet(filename).as[MyRow]
+      fromDisk.collect().toSet shouldBe firstRows.toSet
 
       val actualFiles = list(filename)
       info(s"Files in $filename:\n${actualFiles.mkString("\n")}")
     }
   }
 
-  private def deltaWrite(s: SparkSession, xs: Seq[String], filename: String): Unit = {
+  private def deltaWrite(s: SparkSession, xs: Seq[MyRow], filename: String): Unit = {
     import s.implicits._
-    val df = s.sparkContext.parallelize(xs).toDF("values")
+    val df = s.sparkContext.parallelize(xs).toDF
     df.write.format("delta").mode("overwrite").save(filename)
   }
+}
+
+object DeltaPersistSpec {
+
+  case class MyRow(word: String, value: Int)
+
 }
