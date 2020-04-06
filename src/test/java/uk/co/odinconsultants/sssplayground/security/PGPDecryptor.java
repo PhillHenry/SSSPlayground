85db116a7b749962a7b2b74fc3a0fb1d6cca9dfc
Phillip Henry <PhillHenry@gmail.com>
2020-04-06 16:19:03 +0100

    Some experiments with BouncyCastle PGP.

---
+++ src/test/java/uk/co/odinconsultants/sssplayground/security/PGPDecryptor.java
@@ -0,0 +1,138 @@
+package uk.co.odinconsultants.sssplayground.security;
+
+import org.bouncycastle.openpgp.*;
+import org.bouncycastle.openpgp.jcajce.JcaPGPObjectFactory;
+import org.bouncycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator;
+import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyDecryptorBuilder;
+import org.bouncycastle.openpgp.operator.jcajce.JcePublicKeyDataDecryptorFactoryBuilder;
+import org.bouncycastle.util.io.Streams;
+
+import java.io.*;
+import java.security.NoSuchProviderException;
+import java.util.Iterator;
+
+/**
+ * Largely taken from org.bouncycastle.openpgp.examples.KeyBasedLargeFileProcessor
+ */
+public class PGPDecryptor {
+
+    public static String decryptFile(
+            InputStream in,
+            InputStream keyIn,
+            char[]      passwd)
+            throws IOException, NoSuchProviderException
+    {
+        in = PGPUtil.getDecoderStream(in);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        try
+        {
+            JcaPGPObjectFactory pgpF = new JcaPGPObjectFactory(in);
+            PGPEncryptedDataList enc;
+
+            Object                  o = pgpF.nextObject();
+            //
+            // the first object might be a PGP marker packet.
+            //
+            if (o instanceof PGPEncryptedDataList)
+            {
+                enc = (PGPEncryptedDataList)o;
+            }
+            else
+            {
+                enc = (PGPEncryptedDataList)pgpF.nextObject();
+            }
+
+            //
+            // find the secret key
+            //
+            Iterator it = enc.getEncryptedDataObjects();
+            PGPPrivateKey sKey = null;
+            PGPPublicKeyEncryptedData pbe = null;
+            PGPSecretKeyRingCollection pgpSec = new PGPSecretKeyRingCollection(
+                    PGPUtil.getDecoderStream(keyIn), new JcaKeyFingerprintCalculator());
+
+            while (sKey == null && it.hasNext())
+            {
+                pbe = (PGPPublicKeyEncryptedData)it.next();
+
+                sKey = findSecretKey(pgpSec, pbe.getKeyID(), passwd);
+            }
+
+            if (sKey == null)
+            {
+                throw new IllegalArgumentException("secret key for message not found.");
+            }
+
+            InputStream         clear = pbe.getDataStream(new JcePublicKeyDataDecryptorFactoryBuilder().setProvider("BC").build(sKey));
+
+            JcaPGPObjectFactory    plainFact = new JcaPGPObjectFactory(clear);
+
+            PGPCompressedData   cData = (PGPCompressedData)plainFact.nextObject();
+
+            InputStream         compressedStream = new BufferedInputStream(cData.getDataStream());
+            JcaPGPObjectFactory    pgpFact = new JcaPGPObjectFactory(compressedStream);
+
+            Object              message = pgpFact.nextObject();
+
+            if (message instanceof PGPLiteralData)
+            {
+                PGPLiteralData ld = (PGPLiteralData)message;
+
+                InputStream unc = ld.getInputStream();
+                OutputStream fOut =  new BufferedOutputStream(baos);
+
+                Streams.pipeAll(unc, fOut);
+
+                fOut.close();
+            }
+            else if (message instanceof PGPOnePassSignatureList)
+            {
+                throw new PGPException("encrypted message contains a signed message - not literal data.");
+            }
+            else
+            {
+                throw new PGPException("message is not a simple encrypted file - type unknown.");
+            }
+
+            if (pbe.isIntegrityProtected())
+            {
+                if (!pbe.verify())
+                {
+                    System.err.println("message failed integrity check");
+                }
+                else
+                {
+                    System.err.println("message integrity check passed");
+                }
+            }
+            else
+            {
+                System.err.println("no message integrity check");
+            }
+        }
+        catch (PGPException e)
+        {
+            System.err.println(e);
+            if (e.getUnderlyingException() != null)
+            {
+                e.getUnderlyingException().printStackTrace();
+            }
+        }
+
+        return new String(baos.toByteArray());
+    }
+
+    static PGPPrivateKey findSecretKey(PGPSecretKeyRingCollection pgpSec, long keyID, char[] pass)
+            throws PGPException, NoSuchProviderException
+    {
+        PGPSecretKey pgpSecKey = pgpSec.getSecretKey(keyID);
+
+        if (pgpSecKey == null)
+        {
+            return null;
+        }
+
+        return pgpSecKey.extractPrivateKey(new JcePBESecretKeyDecryptorBuilder().setProvider("BC").build(pass));
+    }
+}
---
+++ src/test/scala/uk/co/odinconsultants/sssplayground/security/DecryptStreamSpec.scala
@@ -0,0 +1,44 @@
+package uk.co.odinconsultants.sssplayground.security
+
+import java.io.{BufferedInputStream, ByteArrayInputStream, DataInputStream, FileInputStream}
+import java.io.File.separator
+import java.security.Security
+
+import org.apache.hadoop.fs.Path
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.scalatest.{Matchers, WordSpec}
+import uk.co.odinconsultants.htesting.hdfs.HdfsForTesting
+import uk.co.odinconsultants.htesting.hdfs.HdfsForTesting.hdfsUri
+import uk.co.odinconsultants.htesting.spark.SparkForTesting.session
+
+class DecryptStreamSpec extends WordSpec with Matchers {
+
+  "Binary file in HDFS" should {
+    "be decrypted" in {
+      Security.addProvider(new BouncyCastleProvider)
+
+      val from  = DecryptStreamSpec.testResourceFQN("text.gpg")
+      val to    = hdfsUri + this.getClass.getSimpleName
+      HdfsForTesting.distributedFS.copyFromLocalFile(new Path(from), new Path(to))
+
+      val decodedRDD = session.sparkContext.binaryFiles(to).map { case (file, stream) =>
+        val inputStream:      DataInputStream       = stream.open()
+        val pkFile:           String                = DecryptStreamSpec.testResourceFQN("alice_privKey.txt").substring(5)
+        val privateKeyStream: BufferedInputStream   = new BufferedInputStream(new FileInputStream(pkFile))
+        PGPDecryptor.decryptFile(inputStream, privateKeyStream, "thisisatest".toCharArray)
+      }
+      println("decoded message = " + decodedRDD.collect().mkString("\n"))
+    }
+  }
+
+
+}
+
+object DecryptStreamSpec {
+
+  def testResourceFQN(filename: String): String = {
+    val tld = this.getClass.getResource(separator)
+    s"${tld}..${separator}..${separator}src${separator}test${separator}resources${separator}$filename"
+  }
+
+}
