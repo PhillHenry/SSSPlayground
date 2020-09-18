3c80b41f85333e0de7ab699853fdd75385f3d068
Phillip Henry <PhillHenry@gmail.com>
2020-09-18 13:14:36 +0100

    Configurable number of partitions.

--- src/test/scala/uk/co/odinconsultants/sssplayground/TestingKafka.scala
+++ src/test/scala/uk/co/odinconsultants/sssplayground/TestingKafka.scala
@@ -1,5 +1,6 @@
 package uk.co.odinconsultants.sssplayground
 
+import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
 import uk.co.odinconsultants.htesting.kafka.{KafkaStarter, ZookeeperSetUp}
 import uk.co.odinconsultants.htesting.local.UnusedPort
 
@@ -11,4 +12,13 @@ object TestingKafka {
   val zooKeeper   = ZookeeperSetUp(hostname, zkPort)
   val kafkaEnv    = new KafkaStarter(hostname, kafkaPort, zkPort, topicName)
   val kafkaServer = kafkaEnv.startKafka()
+
+
+  def createTopic(topicName: String, numPartitions: Int) = {
+    val adminClient = AdminClient.create(kafkaEnv.props)
+    val topics = Seq[NewTopic](new NewTopic(topicName, numPartitions, 1))
+    import scala.collection.JavaConverters._
+    adminClient.createTopics(topics.asJava)
+    adminClient.close()
+  }
 }
--- src/test/scala/uk/co/odinconsultants/sssplayground/streaming/FilePersistenceSpec.scala
+++ src/test/scala/uk/co/odinconsultants/sssplayground/streaming/FilePersistenceSpec.scala
@@ -1,13 +1,14 @@
 package uk.co.odinconsultants.sssplayground.streaming
 
 import org.apache.hadoop.fs.Path
+import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
 import org.apache.spark.sql.{DataFrame, Dataset}
 import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.streaming.Trigger
 import org.scalatest.{Matchers, WordSpec}
 import uk.co.odinconsultants.htesting.hdfs.HdfsForTesting.list
 import uk.co.odinconsultants.htesting.spark.SparkForTesting.session
-import uk.co.odinconsultants.sssplayground.TestUtils
+import uk.co.odinconsultants.sssplayground.{TestUtils, TestingKafka}
 import uk.co.odinconsultants.sssplayground.TestingKafka.{hostname, kafkaPort}
 import uk.co.odinconsultants.sssplayground.joins.RunningAverageMain.{Datum, parsingDatum}
 import uk.co.odinconsultants.sssplayground.kafka.Consuming.streamStringsFromKafka
@@ -25,36 +26,39 @@ class FilePersistenceSpec extends WordSpec with Matchers with TestUtils {
     val processTimeMs = 2000
 
     "not create too many files" in {
+      val numPartitions           = 5
+      TestingKafka.createTopic(topicName, numPartitions)
+
       val dataSet: Dataset[Datum] = streamStringsFromKafka(session, s"$hostname:$kafkaPort", topicName, parsingDatum)
       val someTrigger             = Some(Trigger.ProcessingTime(processTimeMs))
       val df: DataFrame           = dataSet.toDF()
       val query                   = sink.writeStream(df, sinkFile, someTrigger, None)(RowEncoder(df.schema))
 
-      pauseMs(processTimeMs * 2)
       checkNoFilesInHDFS()
 
-      val nFirst        = 10
+      val nFirst        = numPartitions * 100
       val producer      = kafkaProducer()
 
       waitForAll(sendDatumMessages(nFirst, producer, processTimeMs / nFirst))
-      val files: List[String] = listFiles()
+      pauseMs(processTimeMs * 2)
+      val files: List[String] = listParquetFiles()
       withClue(s"files:\n${files.mkString("\n")}") {
-        files.size shouldBe > (0)
+        files.size should be >= numPartitions
       }
     }
 
     def checkNoFilesInHDFS(): List[String] = {
-      val files: List[String] = listFiles()
+      val files: List[String] = listParquetFiles()
       withClue(s"files:\n${files.mkString("\n")}") {
         files should have size 0
       }
       files
     }
 
-    def listFiles(): List[String] = {
+    def listParquetFiles(): List[String] = {
       val files: List[Path] = list(sinkFile)
       println(s"files:\n${files.mkString("\n")}")
-      files.map(_.toString)
+      files.map(_.toString).filter(_.endsWith(".parquet"))
     }
   }
 
